d2a68f9868975509c7b264cca44bf9e621f022c0
Anna Antonova <ms.antanol@yandex.ru>
2020-12-08 01:43:34 +0300

    Добавила функцию ввода пользовательского текста. В html добавила кнопки

--- banner-maker/src/renderer.js
+++ banner-maker/src/renderer.js
@@ -16,26 +16,28 @@ let system = {
 
 window.onload = ()=>{
     // функция подставляет в поле предупреждения размеры окна просмотра.
-    // -2 пикселя для каждого значения, т.к. "пример баннера" обоясан рамкой в 1рх
+    // -2 пикселя для каждого значения, т.к. "пример баннера" опоясан рамкой в 1рх
     
-    let bannerSize = document.querySelector(".example-area");
-    let widthOfBanner = bannerSize.offsetWidth-2,
-        heightOfBanner = bannerSize.offsetHeight-2;
+    let widthOfBanner = canvas_layer1.offsetWidth-2,
+        heightOfBanner = canvas_layer1.offsetHeight-2;
     document.querySelector(".example-area-size").innerText = `${widthOfBanner}*${heightOfBanner}`;
     
     // для корректного отображения, в атрибуте и css канваса должны быть одинаковые размеры
-    canvas_layer1.setAttribute("height", `${heightOfBanner}`);
-    canvas_layer1.setAttribute("width", `${widthOfBanner}`);
-    canvas_layer2.setAttribute("height", `${heightOfBanner}`);
-    canvas_layer2.setAttribute("width", `${widthOfBanner}`);
-    canvas_layer3.setAttribute("height", `${heightOfBanner}`);
-    canvas_layer3.setAttribute("width", `${widthOfBanner}`);
+    canvas_layer1.setAttribute("height", heightOfBanner);
+    canvas_layer1.setAttribute("width", widthOfBanner);
+    canvas_layer2.setAttribute("height", heightOfBanner);
+    canvas_layer2.setAttribute("width", widthOfBanner);
+    canvas_layer3.setAttribute("height", heightOfBanner);
+    canvas_layer3.setAttribute("width", widthOfBanner);
+    
+    document.querySelector("#direction-x").setAttribute("max", widthOfBanner);
+    document.querySelector("#direction-y").setAttribute("max", heightOfBanner)
 
     // Вставка текста "Пример вашего баннера"
     let textBanner = "Пример вашего баннера";
     ctx3.fillStyle = "gray";
-    ctx3.font = "BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif";
-    ctx3.fillText(textBanner, 20, heightOfBanner/2);
+    ctx3.font = "16px BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif";
+    ctx3.fillText(textBanner, 10, heightOfBanner/2);
 
     system.width = widthOfBanner;
     system.height = heightOfBanner;
@@ -54,7 +56,9 @@ function mouseActions(evt) {
             (evt.target.name == 'gradient-type') ||
             (evt.target.name == 'gradient-direction')){
 		reRenderSys(system, 'currentFill', "gradient");
-	} 
+	} else if (evt.target.name == "banner-text"){
+		reRenderSys(system);
+    }
 }
 
 function reRenderSys(obj, elem, action) {
@@ -62,6 +66,7 @@ function reRenderSys(obj, elem, action) {
     
     if (system.currentText == 'Пример вашего баннера'){
         system.currentText = '';
+        ctx3.clearRect(0, 0, system.width, system.height);
     }
 }
 
@@ -92,15 +97,6 @@ function show_sub(index){
     // }
 };
 
-function backgroundZeroing(ctx){
-    // Функция, обнуляющая фон баннера, т.к. в CSS3 фоны накладываются друг на друга (наложение можно сделать в дальнейшем при развитии приложения)
-    // Функция добавляется в начало каждой функции, добавляющей новый фон
-
-    document.querySelector(".example-area").style.background = "";
-
-    ctx.clearRect(0, 0, system.width, system.height);
-}
-
 function groundImage() {
     // Функция, ставящая фоном баннера картинку, которую подгрузит юзер
 
@@ -109,7 +105,8 @@ function groundImage() {
         // если кто-то решит через devTools проставить мультипл в input с загрузкой изображений
         document.querySelector(".error-field").innerText = "Пожалуйста, выберите для баннера только одно изображение!";
     }else{
-        backgroundZeroing(ctx1);
+        // убираем предыдущее изображение + убираем текст, если это "пример баннера"
+        ctx1.clearRect(0, 0, system.width, system.height);
 
         let file = input.files[0],
             imgLayer = document.querySelector(".image-layer"),
@@ -129,7 +126,7 @@ function groundImage() {
 function oneColor(){
     // Функция, ставящая фоном баннера сплошную заливку
 
-    backgroundZeroing(ctx2);
+    ctx2.clearRect(0, 0, system.width, system.height);
     ctx2.fillStyle = document.querySelector("input[name='color-plain']").value;
     ctx2.fillRect(0, 0, system.width, system.height);
 }
@@ -137,20 +134,23 @@ function oneColor(){
 function gradientFill(){
     // Функция, ставящая заливкой градиент, по заданным пользователям параметрам.
     
-    backgroundZeroing(ctx2);
-
-    let exampleArea = document.querySelector(".example-area");
-
+    ctx2.clearRect(0, 0, system.width, system.height);
     let color1 =  document.querySelector("input[name='color-grade1']").value,
         color2 =  document.querySelector("input[name='color-grade2']").value,
         typeGradient = document.querySelector("select[name='gradient-type']").value,
         directionGradient = document.querySelector("select[name='gradient-direction']").value;
     
+    let directionLinear = document.querySelector(".direction-linear"),
+        directionRadial = document.querySelector(".direction-radial");
+
     let gradient;
 
     switch (typeGradient){
         case 'linear':
-            
+            if (directionLinear.classList.contains("direction-hidden")){
+                directionLinear.classList.remove("direction-hidden");
+                directionRadial.classList.add("direction-hidden");
+            }
             if (directionGradient=="top"){
                 gradient = ctx2.createLinearGradient(0,system.height, 0,0);
             }else if (directionGradient=="left"){
@@ -177,6 +177,10 @@ function gradientFill(){
             ctx2.fillRect(0, 0, system.width, system.height);
             break;
         case 'radial':
+            if (directionRadial.classList.contains("direction-hidden")){
+                directionRadial.classList.remove("direction-hidden");
+                directionLinear.classList.add("direction-hidden");
+            }
             gradient = ctx2.createRadialGradient(120,100,system.height, 0,0,30);
             
             // Добавление контрольных точек
@@ -189,12 +193,74 @@ function gradientFill(){
             
             break;
     }
+}
 
+function radialPosition(){
+    let varX = document.querySelector("#direction-x").value,
+        insertX = document.querySelector(".insert-x"),
+        varY = document.querySelector("#direction-y").value,
+        insertY = document.querySelector(".insert-y");
+        
+    let color1 =  document.querySelector("input[name='color-grade1']").value,
+       color2 =  document.querySelector("input[name='color-grade2']").value;
+    
+    insertX.innerText = Math.floor(varX*100/system.width) +"%";
+    insertY.innerText = Math.floor(varY*100/system.height) +"%";
+
+    ctx2.clearRect(0, 0, system.width, system.height);
+
+    gradient = ctx2.createRadialGradient(120,100,system.height, varX,varY,30);
+    
+    // Добавление контрольных точек
+    gradient.addColorStop(0, color1);
+    gradient.addColorStop(1, color2);
+    
+    // Установка стиля заливки и отрисовка прямоугольника градиента
+    ctx2.fillStyle = gradient;
+    ctx2.fillRect(0, 0, system.width, system.height);
 
 }
 
 function addTextInBanner(){
     // Функция, добавляющая(введённый пользователем) текст в баннер
 
-    let textArea = document.querySelector("textarea[name='banner-text']").value;
+    ctx3.clearRect(0, 0, system.width, system.height);
+
+    let userText = document.querySelector("textarea[name='banner-text']"),
+        maxWidth = system.width-10;
+
+    if (Math.floor(ctx3.measureText(userText.value).width) > maxWidth){
+        let lines = getLines(ctx3, userText.value, maxWidth);
+        let startHeight = system.height/2;
+        for (let line of lines){
+            ctx3.fillText(line, 10, startHeight, maxWidth);
+            startHeight += 20;
+        }
+    }else{
+        ctx3.fillStyle = "gray";
+        ctx3.font = "BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif";
+        ctx3.fillText(userText.value, 10, system.height/2, maxWidth);
+        system.currentText = userText.value;
+    }
 };
+
+
+function getLines(ctx, text, maxWidth) {
+    let words = text.split(" ");
+    let lines = [];
+    let currentLine = words[0];
+
+    for (let i = 1; i < words.length; i++) {
+        let word = words[i];
+        let width = ctx.measureText(currentLine + " " + word).width;
+        if (width < maxWidth) {
+            currentLine += " " + word;
+        } else {
+            lines.push(currentLine);
+            currentLine = word;
+        }
+    }
+    lines.push(currentLine);
+    console.log(lines)
+    return lines;
+}
